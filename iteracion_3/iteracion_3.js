5276a1939e3cd1b14274a082e6ebf59c3d37c116
Samuel-G3 <sgarciaine@gmail.com>
2021-12-09 19:26:59 +0100

    first commit

---
+++ iteracion_3/iteracion_3.js
@@ -0,0 +1,25 @@
+//1.1
+const a = 5;
+const b = 10;
+let c = 10 * 5;
+console.log(c);
+
+//1.2
+const a = 10;
+const b = 2;
+console.log(10/2);
+
+//1.3
+console.log(15%9);
+
+//1.4
+const y = 10;
+const z = 5;
+let x = y + z;
+console.log(x);
+
+//1.4
+const y = 10;
+const z = 5;
+const x = y * z;
+console.log(x);
---
+++ iteracion_6/iteracion_6.js
@@ -0,0 +1,9 @@
+
+//1.3
+for (let i = 0; i < 10; i++){
+    if(i == 9){
+    console.log('intentando dormir')
+} else{
+    console.log('Dormido!')
+}
+}
